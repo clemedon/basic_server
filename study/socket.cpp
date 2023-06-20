5afeac166e0a9a29e9da2ae192f535a5ddd880f6
Clément VIDON <cvidon@student.42.fr>
2023-06-20 14:56:13 +0200

    Clear up archi

--- study/socket.cpp
+++ study/socket.cpp
@@ -8,116 +8,60 @@
 #include <poll.h>   // pollfd, poll
 #include <unistd.h>  // close
 #include <iostream>  // cerr, cout
+#include <vector>    // cerr, cout
 
 // LISTENER SOCKET
+// TODO useless since listener go to poll, poll can manage all socket
 
-class ListenerSocket {
+class Pollfd {
  public:
-  explicit ListenerSocket( void ) : _fd( -1 ) {
-    std::cout << "Listener: Construction with socket " << _fd << std::endl;
-    _fd = open( __FILE__, O_RDONLY );
-    std::cout << "Listener: Created socket " << _fd << std::endl;
+  explicit Pollfd( int fd, short events ) {
+    _pollfd.fd = fd;
+    _pollfd.events = events;
+    _pollfd.revents = 0;
   }
 
-  ~ListenerSocket( void ) {
-    closeSocket();
-    std::cout << "Listener: Destruction with socket " << _fd << std::endl;
+  ~Pollfd() {
+    std::cout << "Destruction of " << _pollfd.fd << std::endl;
+    closePollfd();
+    return;
   }
 
-  int& getSocket( void ) const {
-    std::cout << "Listener: Get socket " << _fd << std::endl;
-    return _fd;
-  }
+  int const&    getFd( void ) const { return _pollfd.fd; }
+  pollfd const& getPollfd( void ) const { return _pollfd; }
 
-  void closeSocket( void ) {
-    std::cout << "Listener: Closed socket " << _fd << std::endl;
-    if( _fd != -1 ) {
-      close( _fd );
-      _fd = -1;
+  void closePollfd( void ) {
+    std::cout << "Listener: Closed socket " << _pollfd.fd << std::endl;
+    if( _pollfd.fd != -1 ) {
+      close( _pollfd.fd );
+      _pollfd.fd = -1;
     }
   }
 
  private:
-  int _fd;
-};
-
-// POLL SOCKET
-
-/**
- * @brief       Manage pollfd's fd
- *
- * Create a new fd for struct pollfd, require a listener fd.
- */
-
-class PollSocket : public ISocket {
- public:
-  explicit PollSocket( int fd, short events, short revents )
-    : _pfd.fd( fd ),
-  _pfd.events( events ), _pfd.revents( revents ) {
-    std::cout << "Poll: Construction with socket " << _pfd.fd << std::endl;
-    /* struct sockaddr_storage remoteAddr; */
-    /* socklen_t               remoteAddrLen; */
-    pollfd pfd;  // New pollfd for new connection
-    /* remoteAddrLen = sizeof( remoteAddr ); */
-    /* _pfd.fd = accept( listener, */
-    /*                   reinterpret_cast<struct sockaddr*>( &remoteAddr ), */
-    /*                   &remoteAddrLen ); */
-    /* if( _pfd.fd == -1 ) { */
-    /*   std::cerr << "accept: " << strerror( errno ) << "\n"; */
-    /*   exit( 1 ); */
-    /* } */
-    _pfd.fd = open( __FILE__, O_RDONLY );
-    _pfd.events = POLLIN;
-    _pfd.revents = 0;
-    _pfds.push_back( pfd );
-    std::cout << "Poll: Created socket " << _pfd.fd << std::endl;
-  }
-
-  ~PollSocket() {
-    closeSocket();
-    std::cout << "Poll: Destruction with socket " << _pfd.fd << std::endl;
-  }
-
-  int& getSocket( void ) const {
-    std::cout << "Poll: Get socket " << _fd << std::endl;
-    return _pfd.fd;
-  }
-
-  pollfd& getPfd( void ) const {
-    std::cout << "Poll:" << std::endl;
-    std::cout << "fd = " << _pfd.fd << std::endl;
-    std::cout << "events = " << _events.fd << std::endl;
-    std::cout << "revents = " << _revents.fd << std::endl;
-    return _pfd;
-  }
-
-  void closeSocket( void ) {
-    std::cout << "Poll: Closed socket " << _pfd.fd << std::endl;
-    if( _pfd.fd != -1 ) {
-      close( _pfd.fd );
-      _pfd.fd = -1;
-    }
-  }
-
- private:
-  pollfd _pfd;
+  struct pollfd _pollfd;
 };
 
 int main( void ) {
   std::cout << std::endl;
 
-  ListenerSocket s;
-  s.createSocket();
-  s.getSocket();
-  s.getSocket();
+  std::vector<pollfd> _pfds;  // Pollable file descriptors
 
-  int newsocket = open( __FILE__, O_RDONLY );
+  Pollfd l( open( __FILE__, O_RDONLY ), POLLIN );
+  Pollfd c( open( __FILE__, O_RDONLY ), POLLIN );
 
-  PollSocket s;
-  s.createSocket();
-  s.getSocket();
-  s.getSocket();
+  _pfds.push_back( l.getPollfd() );
+  _pfds.push_back( c.getPollfd() );
+
+  std::cout << "Listener socket: " << l.getFd() << std::endl;
+  std::cout << "Client socket: " << c.getFd() << std::endl;
+
+  int result = poll( reinterpret_cast<pollfd*>( _pfds.data() ), _pfds.size(),
+                     -1 );
+  std::cout << result << std::endl;
+  return result;
 }
-//TODO does pfds really need RAII per socket ? cause isnt alreadi RAII to be
-//managed by the Server class, i mean pfds is an array and when program stop it
-//is cleared by Server.
+
+// TODO does pfds really need RAII per socket ? cause isnt alreadi RAII to be
+// managed by the Server class, i mean pfds is an array and when program stop
+// it is cleared by Server.
